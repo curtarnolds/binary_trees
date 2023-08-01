38f250fe2a429ecfbce5934065c3faf2533e9dca
curtarnolds <afayedormawuli@gmail.com>
2023-08-01 23:11:30 +0000

    7. In-order traversal

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -4,7 +4,7 @@
 /**
  * binary_tree_inorder - Goes through a binary tree using in-order traversal
  * @tree: Pointer to the root of the tree to traverse
- * @func: Pointer to the function to call for each node. The value of the node
+ * @funct: Pointer to the function to call for each node. The value of the node
  * must be passed as a parameter to this function
  * If @tree or @func is NULL, nothing
 */
