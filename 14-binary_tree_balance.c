b1c787d0a818e5c9b3828a48d3d20b19f5bfc3dd
curtarnolds <afayedormawuli@gmail.com>
2023-08-02 19:25:10 +0000

    14. Balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -13,7 +13,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	height_r = tree->right ? 1 + binary_tree_balance(tree->right) : 0;
-	height_l = tree->left ? 1 + binary_tree_balance(tree->left) : 0;
+	height_l = tree->left ? (1 + binary_tree_balance(tree->left)) : 0;
+	height_r = tree->right ? (1 + binary_tree_balance(tree->right)) : 0;
+
 	return (height_l - height_r);
 }
