a28e0a407a2a0450b8f6a32a841fb43f6c8eeb31
curtarnolds <afayedormawuli@gmail.com>
2023-08-02 01:34:03 +0000

    10. Depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -2,8 +2,8 @@
 
 
 /**
- * binary_tree_depth - Measures the height of a binary tree
- * @tree: Pointer to the root node of the tree to be measured
+ * binary_tree_depth - Measures the depth of a node in a binary tree
+ * @tree: Pointer to the node to measure the depth
  * Return: 0 if tree is NULL
 */
 size_t binary_tree_depth(const binary_tree_t *tree)
