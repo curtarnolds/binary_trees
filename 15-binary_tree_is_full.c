7c14c9d6a8ef5fe2244b613a586d5a1805f99871
curtarnolds <afayedormawuli@gmail.com>
2023-08-03 11:30:33 +0000

    15. Is full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -13,6 +13,7 @@ int leaf(const binary_tree_t *node);
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	int check_l, check_r;
+
 	if (tree == NULL)
 		return (0);
 
@@ -26,16 +27,18 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	return (check_r * check_l);
 }
 
-
+/**
+ * leaf - Check if a node is a leaf
+ * @node: Pointer to node to check
+ * Return: 1 if true, else 0
+*/
 int leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
 
 	if ((node->left && node->right) || (!node->left && !node->right))
-	{
 		return (1);
-	}
 	else
 		return (0);
 }
