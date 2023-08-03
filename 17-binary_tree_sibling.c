6bcd8180641b6a49783b16d3532cded4bb6c62ac
curtarnolds <afayedormawuli@gmail.com>
2023-08-03 14:34:16 +0000

    17. Sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -9,11 +9,12 @@
 */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	binary_tree_t *_parent = node->parent;
+	binary_tree_t *_parent;
 
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
 
+	_parent = node->parent;
 	if (_parent->left && _parent->left != node)
 		return (_parent->left);
 	else if (_parent->right && _parent->right != node)
